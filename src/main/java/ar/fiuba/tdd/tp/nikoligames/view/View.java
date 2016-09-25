ab48b39b4bbe8e4d9faf1b757bfaeeeeee87b3a4
tobiasbianchi <tobias.bianchi93@gmail.com>
2016-09-25 05:14:53 -0300

    Start of View Class

---
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/view/View.java
@@ -0,0 +1,22 @@
+package ar.fiuba.tdd.tp.nikoligames.view;
+
+import ar.fiuba.tdd.tp.nikoligames.engine.model.board.Board;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+
+/**
+ * Created by tobias on 24/09/16.
+ */
+public class View extends JFrame {
+
+    public View(String title, Integer sizeX, Integer sizeY) {
+        super(title);
+        setSize(sizeX, sizeY);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new FlowLayout());
+    }
+
+}
