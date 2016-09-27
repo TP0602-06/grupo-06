be904d1d2ed874efe9fc353de26fc8271610c40f
tobiasbianchi <tobias.bianchi93@gmail.com>
2016-09-27 16:51:42 -0300

    Added cell kakuro

---
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/view/cells/KakuroCell.java
@@ -0,0 +1,49 @@
+package ar.fiuba.tdd.tp.nikoligames.view.cells;
+
+import ar.fiuba.tdd.tp.nikoligames.view.ColorSet;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Esta es la representacion grafica de una celda kakuro.
+ */
+public class KakuroCell extends JButton {
+
+    public KakuroCell(){
+        setBackground(ColorSet.INVALID_BACKGROUND);
+        setLayout(new BorderLayout());
+        setEnabled(false);
+    }
+
+    private JLabel makeLabel(Integer value){
+        JLabel label = new JLabel(String.valueOf(value));
+        label.setForeground(ColorSet.NORMAL_BACKGROUND);
+        return label;
+    }
+
+    public void setTop(Integer topValue){
+        JLabel label= makeLabel(topValue);
+        label.setHorizontalAlignment(JLabel.RIGHT);
+        add(label, BorderLayout.PAGE_START);
+
+    }
+
+    public void setBottom(Integer bottomValue){
+        JLabel label= makeLabel(bottomValue);
+        label.setHorizontalAlignment(JLabel.LEFT);
+        add(makeLabel(bottomValue), BorderLayout.PAGE_END);
+    }
+
+    @Override
+    public void paintComponent(Graphics graphic) {
+        super.paintComponent(graphic);
+        Graphics2D g2 = (Graphics2D) graphic;
+        g2.setColor(ColorSet.NORMAL_BACKGROUND);
+        g2.setStroke(new BasicStroke(2f));
+        Rectangle2D bounds = graphic.getClipBounds();
+        g2.draw(new Line2D.Double(0,0,bounds.getWidth(),bounds.getHeight()));
+    }
+}
