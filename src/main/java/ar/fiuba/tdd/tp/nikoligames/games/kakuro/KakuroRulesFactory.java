f767968d14a4a77e9c854f37787ff406d98e1b9d
Matias Sanchez <sanchezmatr@gmail.com>
2016-09-27 22:34:47 -0300

    Se realizo merge con develop

--- src/main/java/ar/fiuba/tdd/tp/nikoligames/games/kakuro/KakuroRulesFactory.java
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/games/kakuro/KakuroRulesFactory.java
@@ -1,7 +1,9 @@
 package ar.fiuba.tdd.tp.nikoligames.games.kakuro;
 
+import ar.fiuba.tdd.tp.nikoligames.engine.model.board.AbstractCell;
 import ar.fiuba.tdd.tp.nikoligames.engine.model.board.Board;
 import ar.fiuba.tdd.tp.nikoligames.engine.model.board.Cell;
+import ar.fiuba.tdd.tp.nikoligames.engine.model.board.Position;
 import ar.fiuba.tdd.tp.nikoligames.engine.model.rules.Rule;
 import ar.fiuba.tdd.tp.nikoligames.engine.model.rules.impl.AdditionRule;
 import ar.fiuba.tdd.tp.nikoligames.engine.model.rules.impl.NoDuplicatesRule;
@@ -25,17 +27,32 @@ public class KakuroRulesFactory {
     }
 
     private Collection<Rule> getColumnClueRules(Integer clue, int firstColumn, int lastColumn, int row) {
-        return this.getClueRules(clue, this.board.getRange(firstColumn, lastColumn, row, row));
+        return this.getClueRules(clue, this.getCells(firstColumn, lastColumn, row, row));
     }
 
     private Collection<Rule> getRowClueRules(Integer clue, int column, int firstRow, int lastRow) {
-        return this.getClueRules(clue, this.board.getRange(column, column, firstRow, lastRow));
+        return this.getClueRules(clue, this.getCells(column, column, firstRow, lastRow));
     }
 
-    private Collection<Rule> getClueRules(Integer clue, Iterable<Cell> cells) {
+    private Collection<Rule> getClueRules(Integer clue, ArrayList<AbstractCell> cells) {
         ArrayList<Rule> rules = new ArrayList<>();
         rules.add(new AdditionRule(clue, cells));
         rules.add(new NoDuplicatesRule(cells));
         return rules;
+    }
+
+    private ArrayList<AbstractCell> getCells(int firstColumn, int lastColumn, int firstRow, int lastRow) {
+        ArrayList<Position> positions = this.getPositions(firstColumn, lastColumn, firstRow, lastRow);
+        return this.board.getArrayOfCells(positions);
+    }
+
+    private ArrayList<Position> getPositions(int firstColumn, int lastColumn, int firstRow, int lastRow) {
+        ArrayList<Position> positions = new ArrayList<>();
+        for (int column = firstColumn; column <= lastColumn; column++) {
+            for (int row = firstRow; row <= lastRow; row++) {
+                positions.add(new Position(column, row));
+            }
+        }
+        return positions;
     }
 }
