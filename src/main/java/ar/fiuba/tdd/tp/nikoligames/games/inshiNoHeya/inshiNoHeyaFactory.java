df08a3ada9cede2431afa1e23e2a12c436b8b122
fedebrasburg <federico.brasburg@gmail.com>
2016-09-29 20:20:34 -0300

    Arreglo el build

--- src/main/java/ar/fiuba/tdd/tp/nikoligames/games/inshiNoHeya/inshiNoHeyaFactory.java
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/games/inshiNoHeya/inshiNoHeyaFactory.java
@@ -3,7 +3,6 @@ package ar.fiuba.tdd.tp.nikoligames.games.inshiNoHeya;
 import ar.fiuba.tdd.tp.nikoligames.engine.model.board.Board;
 import ar.fiuba.tdd.tp.nikoligames.engine.model.factory.AbstractGameFactory;
 import ar.fiuba.tdd.tp.nikoligames.engine.model.rules.Rule;
-import ar.fiuba.tdd.tp.nikoligames.games.kakuro.KakuroRulesFactory;
 
 import java.util.ArrayList;
 
@@ -11,8 +10,15 @@ import java.util.ArrayList;
  * Created by fedebrasburg on 9/29/16.
  */
 public class inshiNoHeyaFactory extends AbstractGameFactory {
+
+    private ArrayList<RegionOfNumber> listOfRegions;
+
+    public  inshiNoHeyaFactory( ArrayList<RegionOfNumber> listOfRegions){
+        this.listOfRegions = listOfRegions;
+    }
+
     protected ArrayList<Rule> getRules(Board board) {
-        inshiNoHeyaRulesFactory inshiRulesFactory = new inshiNoHeyaRulesFactory(board);
+        inshiNoHeyaRulesFactory inshiRulesFactory = new inshiNoHeyaRulesFactory(board,listOfRegions);
         return inshiRulesFactory.getRules();
     }
 }
