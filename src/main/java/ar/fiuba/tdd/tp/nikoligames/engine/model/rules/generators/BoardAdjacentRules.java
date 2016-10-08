d366b2e44142ffbb46da5d3a77ea465ecd90346f
germanchaca <germanchaca@gmail.com>
2016-10-08 19:50:39 -0300

    TOMA Y ARREGLALA

---
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/engine/model/rules/generators/BoardAdjacentRules.java
@@ -0,0 +1,44 @@
+package ar.fiuba.tdd.tp.nikoligames.engine.model.rules.generators;
+
+import ar.fiuba.tdd.tp.nikoligames.engine.model.board.Board;
+import ar.fiuba.tdd.tp.nikoligames.engine.model.board.node.AbstractNode;
+import ar.fiuba.tdd.tp.nikoligames.engine.model.position.ClassicPosition;
+import ar.fiuba.tdd.tp.nikoligames.engine.model.rules.Rule;
+import ar.fiuba.tdd.tp.nikoligames.engine.model.rules.implementations.regionrules.AdjacentRule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by german on 10/8/2016.
+ */
+public abstract class BoardAdjacentRules implements AbstractAdjacentRules {
+
+    protected final Board board;
+
+    public BoardAdjacentRules(Board board){
+        this.board = board;
+    }
+
+    public List<Rule> getAdjacentRules() {
+        List<Rule> rulesGenerated = new ArrayList<Rule>();
+
+        for (int i = 1; i <= board.getRows(); i++) {
+            for (int j = 1; j <= board.getCols(); j++) {
+                ClassicPosition position = new ClassicPosition(i, j);
+                AbstractNode node = board.getNode(position);
+
+                List<AbstractNode> region = new ArrayList<AbstractNode>();
+                region.add(node);
+
+                List<AbstractNode> adjacentList = getAjacentListForNode(i, j);
+
+                Rule rule = new AdjacentRule(region,adjacentList);
+                rulesGenerated.add(rule);
+            }
+        }
+        return rulesGenerated;
+    }
+
+    abstract List<AbstractNode> getAjacentListForNode(int i, int j);
+}
--- src/main/java/ar/fiuba/tdd/tp/nikoligames/engine/model/rules/generators/HorizontalVerticalAdjacentRules.java
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/engine/model/rules/generators/HorizontalVerticalAdjacentRules.java
@@ -12,51 +12,34 @@ import java.util.List;
 /**
  * Created by german on 10/7/2016.
  */
-public class HorizontalVerticalAdjacentRules implements AbstractAdjacentRules {
-    private final Board board;
+public class HorizontalVerticalAdjacentRules extends BoardAdjacentRules {
 
     public HorizontalVerticalAdjacentRules(Board board) {
-        this.board = board;
+        super(board);
     }
+    List<AbstractNode> getAjacentListForNode(int i, int j) {
+        List<AbstractNode> adjacentList = new ArrayList<AbstractNode>();
 
-    public List<Rule> getAdjacentRules() {
-        List<Rule> rulesGenerated = new ArrayList<Rule>();
-
-        for (int i = 1; i <= board.getRows(); i++) {
-            for (int j = 1; j <= board.getCols(); j++) {
-                ClassicPosition position = new ClassicPosition(i, j);
-                AbstractNode node = board.getNode(position);
-
-                List<AbstractNode> region = new ArrayList<AbstractNode>();
-                region.add(node);
-
-                List<AbstractNode> adjacentList = new ArrayList<AbstractNode>();
-
-                if( (j-1) > 0 ){
-                    ClassicPosition positionAdjacentLeft = new ClassicPosition(i, j - 1);
-                    AbstractNode nodeHorizontalLeft = board.getNode(positionAdjacentLeft);
-                    adjacentList.add(nodeHorizontalLeft);
-                }
-                if( (j + 1) < board.getRows() ){
-                    ClassicPosition positionAdjacentRight = new ClassicPosition(i, j + 1);
-                    AbstractNode nodeHorizontalRight = board.getNode(positionAdjacentRight);
-                    adjacentList.add(nodeHorizontalRight);
-                }
-                if( (i-1) > 0 ){
-                    ClassicPosition positionAdjacentTop = new ClassicPosition(i - 1, j);
-                    AbstractNode nodeVerticalTop = board.getNode(positionAdjacentTop);
-                    adjacentList.add(nodeVerticalTop);
-                }
-                if( (i + 1) < board.getRows() ){
-                    ClassicPosition positionAdjacentBottom = new ClassicPosition(i + 1, j);
-                    AbstractNode nodeVerticalBottom = board.getNode(positionAdjacentBottom);
-                    adjacentList.add(nodeVerticalBottom);
-                }
-
-                Rule rule = new AdjacentRule(region,adjacentList);
-                rulesGenerated.add(rule);
-            }
+        if( (j-1) > 0 ){
+            ClassicPosition positionAdjacentLeft = new ClassicPosition(i, j - 1);
+            AbstractNode nodeHorizontalLeft = board.getNode(positionAdjacentLeft);
+            adjacentList.add(nodeHorizontalLeft);
         }
-        return rulesGenerated;
+        if( (j + 1) < board.getRows() ){
+            ClassicPosition positionAdjacentRight = new ClassicPosition(i, j + 1);
+            AbstractNode nodeHorizontalRight = board.getNode(positionAdjacentRight);
+            adjacentList.add(nodeHorizontalRight);
+        }
+        if( (i-1) > 0 ){
+            ClassicPosition positionAdjacentTop = new ClassicPosition(i - 1, j);
+            AbstractNode nodeVerticalTop = board.getNode(positionAdjacentTop);
+            adjacentList.add(nodeVerticalTop);
+        }
+        if( (i + 1) < board.getRows() ){
+            ClassicPosition positionAdjacentBottom = new ClassicPosition(i + 1, j);
+            AbstractNode nodeVerticalBottom = board.getNode(positionAdjacentBottom);
+            adjacentList.add(nodeVerticalBottom);
+        }
+        return adjacentList;
     }
 }
