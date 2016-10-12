6b5f22dde5cd4395a0e1bb9b5b5421351151107b
germanchaca <germanchaca@gmail.com>
2016-10-12 09:43:46 -0300

    fix position

--- src/main/java/ar/fiuba/tdd/tp/nikoligames/engine/model/position/Position.java
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/engine/model/position/Position.java
@@ -11,8 +11,8 @@ public abstract class Position {
     protected int col;
 
     public Position(int row, int col) {
-        this.row = row - 1;
-        this.col = col - 1;
+        this.row = row;
+        this.col = col;
     }
 
     @Override
