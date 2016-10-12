e96b9f678d0ed2ef6a96a5cbe6c17082968bf82d
germanchaca <germanchaca@gmail.com>
2016-10-12 11:00:39 -0300

    regla para contar cuantos nodos de la region están visitados

--- src/main/java/ar/fiuba/tdd/tp/nikoligames/engine/model/rules/implementations/visitrules/RegionCountVisited.java
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/engine/model/rules/implementations/visitrules/RegionCountVisited.java
@@ -1,7 +1,48 @@
 package ar.fiuba.tdd.tp.nikoligames.engine.model.rules.implementations.visitrules;
 
+import ar.fiuba.tdd.tp.nikoligames.engine.model.board.node.AbstractNode;
+import ar.fiuba.tdd.tp.nikoligames.engine.model.rules.implementations.onregion.AbstractRegionRule;
+
+import java.util.List;
+
 /**
  * Created by german on 10/8/2016.
  */
-public class RegionCountVisited {
+public class RegionCountVisited extends AbstractRegionRule {
+
+    private int count = 0;
+    private int countRule;
+
+    public RegionCountVisited(List<AbstractNode> region, String value) {
+        super(region, value);
+        countRule = Integer.parseInt(value);
+    }
+
+    @Override
+    public boolean isActualBroken() {
+        return super.isBroken();
+    }
+
+    @Override
+    public boolean isBroken() {
+        boolean visitedMoreThanCountRule = super.isBroken();
+        if (visitedMoreThanCountRule = true) {
+            return true;
+        }
+        if (count == countRule) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected boolean nodeCondition(AbstractNode node) {
+        if (VisitedHelper.isVisited(node)) {
+            count++;
+            if (count > countRule) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
