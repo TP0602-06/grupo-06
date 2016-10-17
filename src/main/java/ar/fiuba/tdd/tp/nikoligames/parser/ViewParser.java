edc3ee08a8c53b9d4e6736394bdd19a6583a88ba
AndresOtero <oteroandres95@gmail.com>
2016-10-17 16:41:38 -0300

    Agrego una primera version de view parser

---
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/parser/ViewParser.java
@@ -0,0 +1,18 @@
+package ar.fiuba.tdd.tp.nikoligames.parser;
+
+import ar.fiuba.tdd.tp.nikoligames.view.config.ViewConfig;
+import org.json.simple.JSONObject;
+
+/**
+ * Created by Andres on 17/10/2016.
+ */
+public class ViewParser {
+    public static final String VIEW = "view";
+    public static final String IS_CELL_BOARD = "is_cell_board";
+
+    public ViewConfig parseView(JSONObject jsonObject) {
+        JSONObject viewObj = (JSONObject) jsonObject.get(VIEW);
+        boolean is_cell_board = (boolean) viewObj.get(IS_CELL_BOARD);
+        return new ViewConfig(is_cell_board);
+    }
+}
