9114c2c6715aee28b8c4411b365e52f70f4908df
germanchaca <germanchaca@gmail.com>
2016-10-18 11:24:43 -0300

    terminada la acceptance test para INSHI NO HAYA

--- src/test/java/ar/fiuba/tdd/tp/nikoligames/acceptancetests/GameSetup.java
+++ src/test/java/ar/fiuba/tdd/tp/nikoligames/acceptancetests/GameSetup.java
@@ -1,10 +1,14 @@
 package ar.fiuba.tdd.tp.nikoligames.acceptancetests;
 
+import ar.fiuba.tdd.tp.nikoligames.model.board.position.ClassicPosition;
 import ar.fiuba.tdd.tp.nikoligames.model.game.Game;
 import ar.fiuba.tdd.tp.nikoligames.model.game.GameFactory;
 import ar.fiuba.tdd.tp.nikoligames.parser.AbstractParser;
 import ar.fiuba.tdd.tp.nikoligames.parser.ConcreteParser;
 import ar.fiuba.tdd.tp.nikoligames.parser.utils.GameConfig;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Andres on 18/10/2016.
@@ -17,5 +21,20 @@ public class GameSetup {
         GameConfig gameConfig = gameConfigParser.parse();
         Game game = gameFactory.createGame(gameConfig);
         return game;
+    }
+    public static void fillGame(Game game, List<String> values){
+        Iterator<String> iterator = values.iterator();
+        int row = 1;
+        int col = 1;
+        while (iterator.hasNext()){
+            String value = iterator.next();
+            game.changeNodeValue(new ClassicPosition(row, col), value);
+            if (col >= game.getDrawableBoard().getCols()){
+                col = 1;
+                row++;
+            }else {
+                col ++;
+            }
+        }
     }
 }
--- src/test/java/ar/fiuba/tdd/tp/nikoligames/acceptancetests/inshinohaya.java
+++ src/test/java/ar/fiuba/tdd/tp/nikoligames/acceptancetests/inshinohaya.java
@@ -2,12 +2,12 @@ package ar.fiuba.tdd.tp.nikoligames.acceptancetests;
 
 import ar.fiuba.tdd.tp.nikoligames.model.board.position.ClassicPosition;
 import ar.fiuba.tdd.tp.nikoligames.model.game.Game;
-import ar.fiuba.tdd.tp.nikoligames.model.game.GameFactory;
-import ar.fiuba.tdd.tp.nikoligames.parser.AbstractParser;
-import ar.fiuba.tdd.tp.nikoligames.parser.ConcreteParser;
-import ar.fiuba.tdd.tp.nikoligames.parser.utils.GameConfig;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by german on 10/18/2016.
@@ -16,11 +16,9 @@ public class inshinohaya {
 
     public static final String DATA_INSHI_TABLERO1_JSON = "data/inshi/tablero1.json";
 
-
-
     @Test
     public void InshiNoHayaFirstPlay() throws Exception {
-        GameSetup gameSetup =new GameSetup();
+        GameSetup gameSetup = new GameSetup();
         Game game = gameSetup.setup(DATA_INSHI_TABLERO1_JSON);
 
         game.changeNodeValue(new ClassicPosition(1, 1), "3");
@@ -28,4 +26,41 @@ public class inshinohaya {
         Assert.assertTrue(game.getBoardStatus());
         Assert.assertFalse(game.checkWin());
     }
+
+    @Test
+    public void InshiNoHayaWinGame() throws Exception {
+        GameSetup gameSetup = new GameSetup();
+        Game game = gameSetup.setup(DATA_INSHI_TABLERO1_JSON);
+
+        List<String> values = Arrays.asList(
+                "3", "4", "1", "5", "2",
+                "2", "1", "3", "4", "5",
+                "5", "3", "2", "1", "4",
+                "1", "5", "4", "2", "3",
+                "4", "2", "5", "3", "1"
+        );
+        GameSetup.fillGame(game,values);
+
+        Assert.assertTrue(game.getBoardStatus());
+        Assert.assertTrue(game.checkWin());
+    }
+
+    @Test
+    public void InshiNoHayaLooseGame() throws Exception {
+        GameSetup gameSetup = new GameSetup();
+        Game game = gameSetup.setup(DATA_INSHI_TABLERO1_JSON);
+
+        List<String> values = Arrays.asList(
+                "1", "4", "1", "5", "2",
+                "2", "1", "3", "4", "5",
+                "5", "3", "2", "1", "4",
+                "1", "5", "4", "2", "3",
+                "4", "2", "5", "3", "1"
+        );
+        GameSetup.fillGame(game,values);
+
+        Assert.assertFalse(game.getBoardStatus());
+        Assert.assertFalse(game.checkWin());
+    }
+
 }
