d7991e256bf148b132e504f562627abfe1a6f670
fedebrasburg <federico.brasburg@gmail.com>
2016-10-18 21:00:01 -0300

    Arreglo edge

--- src/main/java/ar/fiuba/tdd/tp/nikoligames/model/board/ConcreteBoard.java
+++ src/main/java/ar/fiuba/tdd/tp/nikoligames/model/board/ConcreteBoard.java
@@ -67,6 +67,7 @@ public class ConcreteBoard implements DrawableBoard, Board {
         if (edges.containsKey(edgePosition)) {
             EdgeInterface edge = edges.get(edgePosition);
             edge.erase();
+            edges.remove(edgePosition);
         }
     }
 
