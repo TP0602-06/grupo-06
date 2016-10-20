9af98e3a2cec7d154ea1ccd1075bd60ebb4a5eb8
fedebrasburg <federico.brasburg@gmail.com>
2016-10-20 20:20:20 -0300

    Actividad entrega 2

---
+++ src/test/java/ar/fiuba/tdd/tp/nikoligames/acceptancetests/setup/CheckGame.java
@@ -0,0 +1,32 @@
+package ar.fiuba.tdd.tp.nikoligames.acceptancetests.setup;
+
+import ar.fiuba.tdd.tp.nikoligames.model.game.Game;
+import ar.fiuba.tdd.tp.nikoligames.model.play.AbstractPlay;
+import ar.fiuba.tdd.tp.nikoligames.parser.ArgsParser;
+import ar.fiuba.tdd.tp.nikoligames.parser.PlayParser;
+import ar.fiuba.tdd.tp.nikoligames.reporter.AbstractPlaysReporter;
+import ar.fiuba.tdd.tp.nikoligames.reporter.ReportPlaysJson;
+
+import java.util.List;
+
+/**
+ * Created by fedebrasburg on 10/20/16.
+ */
+public class CheckGame {
+    public static boolean checkGame(String boardPath, String playsPath) throws Exception  {
+        GameSetup gs = new GameSetup();
+        Game game = gs.setup(boardPath);
+        return checkWin(playsPath,game);
+    }
+
+    private static boolean checkWin(String playsPath, Game game) throws Exception {
+
+        List<AbstractPlay> plays = PlayParser.parse(playsPath, game);
+
+        for(AbstractPlay play : plays){
+            play.process();
+        }
+
+        return game.checkWin();
+    }
+}
---
+++ src/test/java/ar/fiuba/tdd/tp/nikoligames/entrega2test/WInTest.java
@@ -0,0 +1,60 @@
+package ar.fiuba.tdd.tp.nikoligames.entrega2test;
+
+import ar.fiuba.tdd.tp.nikoligames.acceptancetests.setup.CheckGame;
+import ar.fiuba.tdd.tp.nikoligames.acceptancetests.setup.GameSetup;
+import ar.fiuba.tdd.tp.nikoligames.model.game.Game;
+import ar.fiuba.tdd.tp.nikoligames.model.play.AbstractPlay;
+import ar.fiuba.tdd.tp.nikoligames.parser.ArgsParser;
+import ar.fiuba.tdd.tp.nikoligames.parser.ConcreteParser;
+import ar.fiuba.tdd.tp.nikoligames.parser.GameParser;
+import ar.fiuba.tdd.tp.nikoligames.parser.PlayParser;
+import ar.fiuba.tdd.tp.nikoligames.parser.utils.GameConfig;
+import ar.fiuba.tdd.tp.nikoligames.reporter.AbstractPlaysReporter;
+import ar.fiuba.tdd.tp.nikoligames.reporter.ReportPlaysJson;
+import ar.fiuba.tdd.tp.nikoligames.view.parentview.GameView;
+import ar.fiuba.tdd.tp.nikoligames.view.parentview.factory.FactoryGameView;
+import ar.fiuba.tdd.tp.nikoligames.view.parentview.factory.FactoryGameViewImplementation;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by fedebrasburg on 10/20/16.
+ */
+public class WInTest {
+
+    private  String boardCountryroadPath= "data/countryroad/tableroEntrega2.json";
+    private  String playsCountryroadPath= "data/countryroad/playsEntrega2.json";
+
+    private  String boardInshiPath= "data/inshi/tablero1.json";
+    private  String playsInshiPath= "data/inshi/plays1.json";
+
+    private  String boardGokigenPath= "data/gokigen/tablero1.json";
+    private  String playsGokigenPath= "data/gokigen/playsentrega2.json";
+
+    private  String boardSlitherlinkPath= "data/slitherlink/tablero2.json";
+    private  String playsSlitherlinkPath= "data/slitherlink/plays2.json";
+
+    @Test
+    public void CountryroadTest() throws Exception {
+        Assert.assertTrue(CheckGame.checkGame(boardCountryroadPath,playsCountryroadPath));
+    }
+
+    @Test
+    public void InshiTest() throws Exception {
+        Assert.assertTrue(CheckGame.checkGame(boardInshiPath,playsInshiPath));
+    }
+
+    @Test
+    public void GokigenTest() throws Exception {
+        Assert.assertTrue(CheckGame.checkGame(boardGokigenPath,playsGokigenPath));
+    }
+
+    @Test
+    public void SlitherlinkTest() throws Exception {
+        Assert.assertTrue(CheckGame.checkGame(boardSlitherlinkPath,playsSlitherlinkPath));
+    }
+
+
+}
